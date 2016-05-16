06ab89c4a631b93282238fa11a931f504cf5b4fd
Brycen Dorsay <bdorsay@rbauction.com>
2016-05-16 14:50:19 -0700

    Removed web volume from db

--- web/conf.d
+++ web/conf.d
@@ -1,5 +1,5 @@
 server {
     listen                  80;
     server_name             idealist.dorsaydevelopment.ca;
-    root                    /usr/share/nginx/html;
+    root                    /src;
 }
